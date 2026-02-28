c7572aa118b5a33fd0f838c2328652d42adc9a47
Dipak Sahani <sahanidipak671@gmail.com>
2026-02-28 06:31:51 +0530

    add

--- main.py
+++ main.py
@@ -765,4 +765,5 @@ async def normalize_raw_prescription(payload: NormalizationRawPayload):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
